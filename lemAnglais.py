4d18b90f3dbe91ecdbc8a2c8b96d7f67c5a797ed
moucheAtapette <l.grandjean93@gmail.com>
2025-03-06 00:00:55 +0100

    commit pré-code lemnatization des texts

--- lemAnglais.py
+++ lemAnglais.py
@@ -1,6 +1,7 @@
 import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
+from nltk.tokenize import word_tokenize
 
 # Téléchargement des ressources nécessaires
 nltk.download('wordnet')
@@ -23,23 +24,18 @@ def nltk_pos_tagger(nltk_tag):
     else:          
         return None
 
-def lemmatize_english(sentence):
 
-    nltk_tagged = nltk.pos_tag(nltk.word_tokenize(sentence))  
-    wordnet_tagged = map(lambda x: (x[0], nltk_pos_tagger(x[1])), nltk_tagged)
-    lemmatized_sentence = []
-    
-    for word, tag in wordnet_tagged:
-        if tag is None:
-            lemmatized_sentence.append(word)
-        else:        
-            lemmatized_sentence.append(lemmatizer.lemmatize(word, tag))
-    return " ".join(lemmatized_sentence) 
-    """
-    words = nltk.word_tokenize(sentence)  # Tokenisation
-    lemmatized_words = [lemmatizer.lemmatize(word) for word in words]
-    return ' '.join(lemmatized_words)
-    """
+def lemmatize_english(words):
+    lemmatizer = WordNetLemmatizer()
+    a = []
+    tokens = word_tokenize(words)
+    for token in tokens:
+        lemmetized_word = lemmatizer.lemmatize(token)
+        a.append(lemmetized_word)
+    sentence = " ".join(a)
+    return sentence
+
+
 
 def lemmatize_corpus(input_file, output_file, lemmatizer_func):
     with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', encoding='utf-8') as outfile:
@@ -48,7 +44,7 @@ def lemmatize_corpus(input_file, output_file, lemmatizer_func):
             outfile.write(lemmatized_line + '\n')
 
 # Exemple pour l'anglais (Europarl)
-lemmatize_corpus('data/provided-corpus/a.en', 'data/created/a.en', lemmatize_english)
+lemmatize_corpus('data/provided-corpus/Europarl_test_500.en', 'data/created/Europarl_test_500.en', lemmatize_english)
 
 print(lemmatizer.lemmatize("running"))
 print(lemmatizer.lemmatize("better", pos="a"))  # Ajoute un "pos" pour spécifier que c'est un adjectif
--- lemFrancais.py
+++ lemFrancais.py
@@ -1,8 +1,6 @@
 from french_lefff_lemmatizer.french_lefff_lemmatizer import FrenchLefffLemmatizer
 
-
-# Spécifiez manuellement l'emplacement du fichier LEFFF
-french_lemmatizer = FrenchLefffLemmatizer(LEFFF_FILE_STORAGE='/data/provided-corpus/Europarl_test_500.fr')
+french_lemmatizer = FrenchLefffLemmatizer()
 
 
 
@@ -18,4 +16,16 @@ def lemmatize_corpus(input_file, output_file, lemmatizer_func):
             outfile.write(lemmatized_line + '\n')
 
 # Exemple pour le français (Europarl)
-lemmatize_corpus('Europarl_train_100k.fr', 'Europarl_train_100k_lemmatized.fr', lemmatize_french)
+lemmatize_corpus('data/provided-corpus/Europarl_train_10k.fr', 'data/created/Europarl_train_10k_lemmatized.fr', lemmatize_french)
+
+
+def lemmatize_frenchTEST(sentence):
+    words = sentence.split()  # Séparer la phrase en mots
+    lemmatized_words = [french_lemmatizer.lemmatize(word) for word in words]
+    print(f"Original sentence: {sentence}")
+    print(f"Lemmatized sentence: {' '.join(lemmatized_words)}")
+    return ' '.join(lemmatized_words)
+
+
+sentence = "Les chiens mangeaient les croquettes dans le jardin."
+print(lemmatize_frenchTEST(sentence))
