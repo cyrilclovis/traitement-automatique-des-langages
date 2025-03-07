875b58fc5c8804474fac477d08d9437cd21b75f6
moucheAtapette <l.grandjean93@gmail.com>
2025-03-07 15:35:46 +0100

    [INSTABLE] environnemt de test instable pour gpu_ranks=1

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from src.pipelines.pipeline_factory import PipelineFactory
 
 if __name__ == "__main__":
     classic_pipeline = PipelineFactory.get_i2_all_run(
-        folder_base_path= "./data/partieII",
+        folder_base_path= "./data/partieIII",
         language_codes= ["en", "fr"],
-        yaml_config_path_run1= "./config/partieII-europarl_en_fr.yaml",
-        yaml_config_path_run2= "./config/partieII-mix-europarl_emea_en_fr.yaml"
+        yaml_config_path_run1= "./config/partieIII-europarl_en_fr.yaml",
+        yaml_config_path_run2= "./config/partieIII-mix-europarl_emea_en_fr.yaml",
+        useLemmatizer = True
     )
     classic_pipeline.execute()
